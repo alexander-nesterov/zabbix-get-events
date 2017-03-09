257f41d094787804b85c244c3b985cbde8828b92
Alexander Nesterov <0x12ee705@gmail.com>
2017-03-09 16:50:19 +0700

    First commit

---
+++ get_events.pl
@@ -0,0 +1,273 @@
+#!/usr/bin/perl
+
+#documentation
+#https://www.zabbix.com/documentation/3.0/ru/manual/api/reference/event/get
+#http://www.onlineconversion.com/unix_time.htm
+
+#event.value
+#0 - OK
+#1 - PROBLEM
+
+use strict;
+use warnings;
+
+use JSON::XS qw(encode_json decode_json);
+use Excel::Writer::XLSX;
+use MIME::Lite;
+use JSON::RPC::Client;
+use Data::Dumper;
+use utf8;
+
+binmode(STDOUT,':utf8');
+
+#================================================================
+#Constants
+#================================================================
+
+#ZABBIX
+use constant ZABBIX_USER	=> 'Admin';
+use constant ZABBIX_PASSWORD	=> 'zabbix';
+use constant ZABBIX_SERVER	=> 'zabbix';
+
+#MAIL
+use constant FROM		=> 'report\@your_domain';
+use constant RECIPIENT		=> 'info\@your_domain';
+use constant SUBJECT		=> 'zabbix events';
+use constant SMTP_SERVER	=> '127.0.0.1';
+
+#================================================================
+##Global variables
+#================================================================
+my $ZABBIX_AUTH_ID;
+
+my %event_value = (
+		    0 => 'OK',
+		    1 => 'PROBLEM'
+);
+
+my %event_source = (
+		    0 => 'Trigger',
+		    1 => 'Discovery rule',
+		    2 => 'Auto-registration',
+		    3 => 'Internal event'
+);
+
+my %event_acknowledged = (
+		    0 => 'NO',
+		    1 => 'YES'
+);
+
+my %trigger_priority = (
+		    0 => 'Not classified',
+		    1 => 'Information',
+		    2 => 'Warning',
+		    3 => 'Average',
+		    4 => 'High',
+		    5 => 'Disaster'
+);
+
+#================================================================
+main();
+
+#================================================================
+sub main
+{
+    system('clear');
+
+    if (zabbix_auth() != 0)
+    {
+	zabbix_get_events();
+	zabbix_logout();
+    }
+}
+
+#================================================================
+sub zabbix_auth
+{
+    my %data;
+
+    $data{'jsonrpc'} = '2.0';
+    $data{'method'} = 'user.login';
+    $data{'params'}{'user'} = ZABBIX_USER;
+    $data{'params'}{'password'} = ZABBIX_PASSWORD;
+    $data{'id'} = 1;
+
+    my $response = send_to_zabbix(\%data);
+
+    if (!defined($response))
+    {
+	print "Authentication failed, zabbix server: ". ZABBIX_SERVER . "\n";
+	return 0;
+    }
+
+    $ZABBIX_AUTH_ID = $response->content->{'result'};
+
+    print "Authentication successful. Auth ID: $ZABBIX_AUTH_ID\n";
+
+    undef $response;
+
+    return 1;
+}
+
+#================================================================
+sub zabbix_logout
+{
+    my %data;
+
+    $data{'jsonrpc'} = '2.0';
+    $data{'method'} = 'user.logout';
+    $data{'params'} = [];
+    $data{'auth'} = $ZABBIX_AUTH_ID;
+    $data{'id'} = 1;
+
+    my $response = send_to_zabbix(\%data);
+
+    if (!defined($response))
+    {
+        print "Logout failed, zabbix server: " . ZABBIX_SERVER . "\n";
+        return 0;
+    }
+
+    print "Logout successful. Auth ID: $ZABBIX_AUTH_ID\n";
+
+    undef $response;
+}
+
+
+#================================================================
+sub send_to_zabbix
+{
+    my $json = shift;
+
+    my $response;
+
+    my $url = "http://" . ZABBIX_SERVER . "/api_jsonrpc.php";
+
+    my $client = new JSON::RPC::Client;
+
+    $response = $client->call($url, $json);
+
+    #print Dumper $response;
+
+    return $response;
+}
+
+#================================================================
+sub zabbix_get_events
+{
+    my %data;
+
+    $data{'jsonrpc'} = '2.0';
+    $data{'method'} = 'event.get';
+
+    #Possible values:
+    #0 - event created by a trigger
+    #1 - event created by a discovery rule
+    #2 - event created by active agent auto-registration
+    #3 - internal event
+    $data{'source'} = 0;
+
+    $data{'params'}{'output'} = 'extend';
+
+    #Return only events that have been created after or at the given time
+    $data{'params'}{'time_from'} = '1488931200';
+
+    #Return only events that have been created before or at the given time
+    $data{'params'}{'time_till'} = get_current_time();
+
+    $data{'params'}{'sortorder'} = 'DESC'; #DESC or ASC
+
+    #Sort the result by the given properties
+    #Possible values are: eventid, objectid and clock
+    $data{'params'}{'sortfield'} = ['clock', 'eventid'];
+
+    $data{'params'}{'limit'} = 2;
+
+    $data{'auth'} = $ZABBIX_AUTH_ID;
+    $data{'id'} = 1;
+
+    #print Dumper \%data;
+
+    my $response = send_to_zabbix(\%data);
+
+
+   foreach my $event(@{$response->content->{'result'}}) 
+   {
+	my $eventid = $event->{'eventid'};
+	my $objectid = $event->{'objectid'};
+	my $clock = $event->{'clock'}; #Time when the event was created
+	my $value =  $event_value{$event->{'value'}};
+	my $source = $event_source{$event->{'source'}};
+	my $acknowledged = $event_acknowledged{$event->{'acknowledged'}}; #If set to true return only acknowledged events
+
+	print "Clock => " . unix_time_to_date($clock) . "\n";
+	print "ObjectID => $objectid\n";
+	print "Status => $value\n";
+	print "Source => $source\n";
+	print "Acknowledged => $acknowledged\n";
+
+	zabbix_get_trigger($objectid);
+
+	print "==========================================================\n";
+   }
+}
+
+#================================================================
+sub zabbix_get_trigger
+{
+    my $triggerid = shift;
+    my %data;
+
+    $data{'jsonrpc'} = '2.0';
+    $data{'method'} = 'trigger.get';
+
+    $data{'params'}{'output'} = 'extend';
+    $data{'params'}{'triggerids'} = $triggerid;
+    $data{'params'}{'selectHosts'} = ['hostid', 'name', 'status'];
+
+    $data{'auth'} = $ZABBIX_AUTH_ID;
+    $data{'id'} = 1;
+
+    my $response = send_to_zabbix(\%data);
+
+    print Dumper $response;
+
+    foreach my $trigger(@{$response->content->{'result'}})
+    {
+        my $triggerid = $trigger->{'triggerid'};
+	my $description = $trigger->{'description'};
+	my $comments = $trigger->{'comments'};
+
+	#Severity of the trigger
+	#Possible values are:
+	#0 - (default) not classified;
+	#1 - information;
+	#2 - warning;
+	#3 - average;
+	#4 - high;
+	#5 - disaster.
+	my $priority = $trigger_priority{$trigger->{'priority'}};
+
+        print "TriggerID => $triggerid\n";
+	print "Description => $description\n";
+	print "Comments => $comments\n";
+	print "Priority => $priority\n";
+   }
+}
+
+#================================================================
+sub get_current_time
+{
+    return time;
+}
+
+#================================================================
+sub unix_time_to_date
+{
+    my $unix_time = shift;
+
+    return localtime($unix_time);
+}
+
+
+
